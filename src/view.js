b4d37944bea61334388d4f382016874e8eb70b7e
sergeyshalygin <shalygin.javascript@gmail.com>
2023-05-09 13:23:57 +0300

    change view added onChange new array posts

--- src/view.js
+++ src/view.js
@@ -2,28 +2,24 @@ const renderPosts = (state, div, i18nInstance) => {
 	const ul = document.createElement('ul');
 	ul.classList.add('list-group', 'border-0', 'rounded-0');
 
-	state.contentValue.posts.forEach((post) => {
+	state.content.posts.forEach((post) => {
+		const { title, link, id } = post;
+
 		const li = document.createElement('li');
 		li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
 
 		const a = document.createElement('a');
-		a.classList.add(state.uiState.visitedLinksId.has(post.id) ? ('fw-normal link-secondary') : 'fw-bold');
-
-
-		a.setAttribute('href', post.link);
-		a.setAttribute('data-id', post.id);
+		a.classList.add(state.uiState.visitedLinksIds.has(id) ? ('fw-normal', 'link-secondary') : 'fw-bold');
+		a.setAttribute('href', link);
+		a.setAttribute('data-id', id);
 		a.setAttribute('target', '_blank');
 		a.setAttribute('rel', 'noopener noreferrer');
-		a.textContent = post.title;
-
-		a.addEventListener('click', () => {
-			state.uiState.visitedLinksId.add(post.id);
-		})
+		a.textContent = title;
 
 		const button = document.createElement('button');
 		button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
 		button.setAttribute('type', 'button');
-		button.setAttribute('data-id', post.id);
+		button.setAttribute('data-id', id);
 		button.setAttribute('data-bs-toggle', 'modal');
 		button.setAttribute('data-bs-target', '#modal');
 		button.textContent = i18nInstance.t('button');
@@ -39,17 +35,19 @@ const renderFeeds = (state, div) => {
 	const ul = document.createElement('ul');
 	ul.classList.add('list-group', 'border-0', 'rounded-0');
 
-	state.contentValue.feeds.forEach((feed) => {
+	state.content.feeds.forEach((feed) => {
+		const { title, description } = feed;
+
 		const li = document.createElement('li');
 		li.classList.add('list-group-item', 'border-0', 'border-end-0');
 
 		const h3 = document.createElement('h3');
 		h3.classList.add('h6', 'm-0');
-		h3.textContent = feed.title;
+		h3.textContent = title;
 
 		const p = document.createElement('p');
 		p.classList.add('m-0', 'small', 'text-black-50');
-		p.textContent = feed.description;
+		p.textContent = description;
 
 		li.append(h3, p);
 		ul.append(li);
@@ -75,16 +73,22 @@ const createContainer = (type, elements, state, i18nInstance) => {
 	divCard.append(divCardBody);
 	elements[type].append(divCard);
 
-	if (type === 'posts') renderPosts(state, divCard, i18nInstance);
-	if (type === 'feeds') renderFeeds(state, divCard);
+	if (type === 'posts') {
+		renderPosts(state, divCard, i18nInstance);
+	}
+
+	if (type === 'feeds') {
+		renderFeeds(state, divCard);
+	}
 };
 
 const renderModalWindow = (elements, state, postId) => {
-	const currentPost = state.contentValue.posts.find(({ id }) => id === postId);
+	const currentPost = state.content.posts.find(({ id }) => id === postId);
+	const { title, description, link } = currentPost;
 
-	elements.modal.title.textContent = currentPost.title;
-	elements.modal.body.textContent = currentPost.description;
-	elements.modal.button.setAttribute('href', currentPost.link);
+	elements.modal.title.textContent = title;
+	elements.modal.body.textContent = description;
+	elements.modal.button.setAttribute('href', link);
 };
 
 const handlerSuccessFinish = (elements, i18nInstance) => {
@@ -95,9 +99,10 @@ const handlerSuccessFinish = (elements, i18nInstance) => {
 	elements.button.removeAttribute('disabled');
 
 	elements.input.removeAttribute('readonly');
-	elements.input.focus();
-	elements.form.reset();
 	elements.input.classList.remove('is-invalid');
+	elements.input.focus();
+
+	elements.form.reset();
 };
 
 const handlerFinishWitnError = (elements, error, i18nInstance) => {
@@ -146,11 +151,15 @@ export default (elements, state, i18nInstance) => (path, value) => {
 			renderModalWindow(elements, state, value);
 			break;
 
-		case 'contentValue.posts':
+		case 'uiState.visitedLinksIds':
 			createContainer('posts', elements, state, i18nInstance);
 			break;
 
-		case 'contentValue.feeds':
+		case 'content.posts':
+			createContainer('posts', elements, state, i18nInstance);
+			break;
+
+		case 'content.feeds':
 			createContainer('feeds', elements, state, i18nInstance);
 			break;
 
